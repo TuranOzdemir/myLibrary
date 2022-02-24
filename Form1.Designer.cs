fecc3543d057966c69b170c78f16289f2b53ba6c
TuranOzdemir <turanozdmr@outlook.com>
2022-02-24 19:18:30 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace myLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sayfa_tbx = new System.Windows.Forms.TextBox();
+            this.yazar_tbx = new System.Windows.Forms.TextBox();
+            this.yayin_tbx = new System.Windows.Forms.TextBox();
+            this.ad_tbx = new System.Windows.Forms.TextBox();
+            this.sira_tbx = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ekle_btn = new DevExpress.XtraEditors.SimpleButton();
+            this.sil_btn = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.clear_btn = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.simpleButton2);
+            this.groupBox1.Controls.Add(this.clear_btn);
+            this.groupBox1.Controls.Add(this.sil_btn);
+            this.groupBox1.Controls.Add(this.ekle_btn);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.sayfa_tbx);
+            this.groupBox1.Controls.Add(this.yazar_tbx);
+            this.groupBox1.Controls.Add(this.yayin_tbx);
+            this.groupBox1.Controls.Add(this.ad_tbx);
+            this.groupBox1.Controls.Add(this.sira_tbx);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(557, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 228);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kitap Ekle";
+            // 
+            // sayfa_tbx
+            // 
+            this.sayfa_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayfa_tbx.Location = new System.Drawing.Point(110, 145);
+            this.sayfa_tbx.Name = "sayfa_tbx";
+            this.sayfa_tbx.Size = new System.Drawing.Size(266, 24);
+            this.sayfa_tbx.TabIndex = 15;
+            // 
+            // yazar_tbx
+            // 
+            this.yazar_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yazar_tbx.Location = new System.Drawing.Point(110, 85);
+            this.yazar_tbx.Name = "yazar_tbx";
+            this.yazar_tbx.Size = new System.Drawing.Size(266, 24);
+            this.yazar_tbx.TabIndex = 14;
+            // 
+            // yayin_tbx
+            // 
+            this.yayin_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yayin_tbx.Location = new System.Drawing.Point(110, 115);
+            this.yayin_tbx.Name = "yayin_tbx";
+            this.yayin_tbx.Size = new System.Drawing.Size(266, 24);
+            this.yayin_tbx.TabIndex = 13;
+            // 
+            // ad_tbx
+            // 
+            this.ad_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ad_tbx.Location = new System.Drawing.Point(110, 55);
+            this.ad_tbx.Name = "ad_tbx";
+            this.ad_tbx.Size = new System.Drawing.Size(266, 24);
+            this.ad_tbx.TabIndex = 12;
+            // 
+            // sira_tbx
+            // 
+            this.sira_tbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sira_tbx.Location = new System.Drawing.Point(110, 25);
+            this.sira_tbx.Name = "sira_tbx";
+            this.sira_tbx.Size = new System.Drawing.Size(266, 24);
+            this.sira_tbx.TabIndex = 11;
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(539, 426);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 43;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kitap Adı";
+            this.columnHeader2.Width = 141;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Yazar";
+            this.columnHeader3.Width = 83;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Yayın Evi";
+            this.columnHeader4.Width = 94;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sayfa Sayısı";
+            this.columnHeader5.Width = 84;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 18);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Kitap Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 18);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Yayın Evi :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(50, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 18);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Yazar :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(62, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Sıra :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(8, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 18);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Sayfa Sayısı :";
+            // 
+            // ekle_btn
+            // 
+            this.ekle_btn.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Success;
+            this.ekle_btn.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ekle_btn.Appearance.Options.UseBackColor = true;
+            this.ekle_btn.Appearance.Options.UseFont = true;
+            this.ekle_btn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("ekle_btn.ImageOptions.Image")));
+            this.ekle_btn.Location = new System.Drawing.Point(289, 175);
+            this.ekle_btn.Name = "ekle_btn";
+            this.ekle_btn.Size = new System.Drawing.Size(87, 42);
+            this.ekle_btn.TabIndex = 23;
+            this.ekle_btn.Text = "Ekle";
+            this.ekle_btn.Click += new System.EventHandler(this.ekle_btn_Click);
+            // 
+            // sil_btn
+            // 
+            this.sil_btn.Appearance.BackColor = DevExpress.LookAndFeel.DXSkinColors.FillColors.Danger;
+            this.sil_btn.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sil_btn.Appearance.Options.UseBackColor = true;
+            this.sil_btn.Appearance.Options.UseFont = true;
+            this.sil_btn.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("sil_btn.ImageOptions.Image")));
+            this.sil_btn.Location = new System.Drawing.Point(208, 175);
+            this.sil_btn.Name = "sil_btn";
+            this.sil_btn.Size = new System.Drawing.Size(72, 42);
+            this.sil_btn.TabIndex = 24;
+            this.sil_btn.Text = "Sil";
+            this.sil_btn.Click += new System.EventHandler(this.sil_btn_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = System.Drawing.Color.Orange;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(557, 246);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(382, 42);
+            this.simpleButton1.TabIndex = 25;
+            this.simpleButton1.Text = "Kitapları Listele";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // clear_btn
+            // 
+            this.clear_btn.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.clear_btn.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.clear_btn.Appearance.Options.UseBackColor = true;
+            this.clear_btn.Appearance.Options.UseFont = true;
+            this.clear_btn.Location = new System.Drawing.Point(6, 175);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(78, 42);
+            this.clear_btn.TabIndex = 25;
+            this.clear_btn.Text = "Temizle";
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Appearance.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.simpleButton2.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.simpleButton2.Appearance.Options.UseBackColor = true;
+            this.simpleButton2.Appearance.Options.UseFont = true;
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(90, 175);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(112, 42);
+            this.simpleButton2.TabIndex = 26;
+            this.simpleButton2.Text = "Düzenle";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(951, 450);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox sayfa_tbx;
+        private System.Windows.Forms.TextBox yazar_tbx;
+        private System.Windows.Forms.TextBox yayin_tbx;
+        private System.Windows.Forms.TextBox ad_tbx;
+        private System.Windows.Forms.TextBox sira_tbx;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private DevExpress.XtraEditors.SimpleButton ekle_btn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton sil_btn;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton clear_btn;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace myLibrary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connect = new SqlConnection("Data Source=DESKTOP-INQ1LMR\\SQLEXPRESS;Initial Catalog=Kutuphane;Integrated Security=True");
+
+        private void showInfo()
+        {
+            connect.Open();
+            SqlCommand komut = new SqlCommand("select *from kitaplar", connect);
+            SqlDataReader oku = komut.ExecuteReader();
+            while (oku.Read())
+            {
+                ListViewItem add = new ListViewItem();
+                add.Text = oku["Id"].ToString();
+                add.SubItems.Add(oku["kitapAd"].ToString());
+                add.SubItems.Add(oku["yazar"].ToString());
+                add.SubItems.Add(oku["yayınEvi"].ToString());
+                add.SubItems.Add(oku["sayfa"].ToString());
+                listView1.Items.Add(add);
+            }
+            connect.Close();
+        }
+
+        
+
+        private void ekle_btn_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+            
+            SqlCommand komut = new SqlCommand("insert into kitaplar (Id,kitapAd,yazar,yayınEvi,sayfa) values('" + sira_tbx.Text.ToString() + "','" + ad_tbx.Text.ToString() + "','" + yazar_tbx.Text.ToString() + "','" + yayin_tbx.Text.ToString() + "','" + sayfa_tbx.Text.ToString() + "')", connect);
+            komut.ExecuteNonQuery();
+            connect.Close();
+            sira_tbx.Clear();
+            ad_tbx.Clear();
+            yazar_tbx.Clear();
+            yayin_tbx.Clear();
+            sayfa_tbx.Clear();
+            listView1.Items.Clear();
+            showInfo();
+        }
+      
+        int id = 0;
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            id = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+            sira_tbx.Text = listView1.SelectedItems[0].SubItems[0].Text;
+            ad_tbx.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            yazar_tbx.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            yayin_tbx.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            sayfa_tbx.Text = listView1.SelectedItems[0].SubItems[4].Text;
+        }
+
+        private void sil_btn_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            connect.Open();
+            SqlCommand komut = new SqlCommand("DELETE FROM kitaplar where Id =(" + id + ")", connect);
+            komut.ExecuteNonQuery();
+            
+            connect.Close();
+            showInfo();
+        }
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            showInfo();
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            sira_tbx.Clear();
+            ad_tbx.Clear();
+            yazar_tbx.Clear();
+            yayin_tbx.Clear();
+            sayfa_tbx.Clear();
+
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            connect.Open();
+            SqlCommand komut = new SqlCommand("UPDATE kitaplar set Id='"+sira_tbx.Text.ToString()+"', kitapAd='"+ad_tbx.Text.ToString()+ "', yazar='"+yazar_tbx.Text.ToString()+ "', yayınEvi='"+yayin_tbx.Text.ToString()+ "', sayfa='"+sayfa_tbx.Text.ToString()+"'WHERE Id=(" +id+")",connect);
+            komut.ExecuteNonQuery();
+            connect.Close();
+            showInfo();
+        }
+    }
+}
